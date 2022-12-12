40539b587b749c19ac7b365d81a66d26d26cb921
Moe Jangda <moe@tbd.email>
2022-12-13 02:40:12 +0500

    return something from calling `web5.dwn.requestAccess`

--- todolist-v2/web5-wallet/content-scripts/web5.js
+++ todolist-v2/web5-wallet/content-scripts/web5.js
@@ -3,7 +3,15 @@ console.log('injected web5 onto window!');
 window.web5 = {
   dwn: {
     requestAccess: async function () {
-      await window.web5.send('DWN_REQUEST_ACCESS');
+      const { data, errors } = await window.web5.send('DWN_REQUEST_ACCESS');
+
+      // TODO: better error handling.
+      if (errors) {
+        throw new Error(errors);
+      }
+
+      // TODO: check for presence of result.errors
+      return data;
     },
 
     processMessage: async function (message) {
